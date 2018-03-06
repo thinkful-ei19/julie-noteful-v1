2646ffc6be96e8749c68abe0f733b32bc934734f
Julie K <julieskim160@gmail.com>
2018-03-06 13:52:25 -0800

    get notes with search

--- server.js
+++ server.js
@@ -5,24 +5,41 @@ const express = require('express');
 const morgan = require('morgan');
 
 const data = require('./db/notes');
+const simDB = require('./db/simDB');  
+const notes = simDB.initialize(data); 
+
 const app = express();
 
 app.use(express.static('public'));
 app.use(morgan('common'));
 
-//verbose solution
-app.get('/api/notes', (req, res) => {
-  const searchTerm = req.query.searchTerm; 
-  if (searchTerm) {
-    let filteredList = data.filter(function(item) {
-      return item.title.includes(searchTerm);
-    });
-    res.json(filteredList);
-  } else {
-    res.json(data);
-  }
+
+app.get('/api/notes', (req, res, next) => {
+  const { searchTerm } = req.query;
+  
+  notes.filter(searchTerm, (err, list) => {
+    if (err) {
+      return next(err); // goes to error handler
+    }
+    res.json(list); // responds with filtered array
+  });
 });
 
+
+//verbose solution replaced by above
+// app.get('/api/notes', (req, res) => {
+//   const searchTerm = req.query.searchTerm; 
+//   if (searchTerm) {
+//     let filteredList = data.filter(function(item) {
+//       return item.title.includes(searchTerm);
+//     });
+//     res.json(filteredList);
+//   } else {
+//     res.json(data);
+//   }
+// });
+
+//example for error
 app.get('/boom', (req, res, next) => {
   throw new Error('Boom!!');
 });
@@ -44,13 +61,14 @@ app.get('/api/notes/:id', (req, res) => {
   res.json(data.find(item => item.id === Number(req.params.id)));
 });
 
-
+//404 error handler
 app.use(function (req, res, next) {
   var err = new Error('Not Found');
   err.status = 404;
   res.status(404).json({ message: 'Not Found' });
 });
 
+//custom error handler
 app.use(function (err, req, res, next) {
   res.status(err.status || 500);
   res.json({
@@ -65,3 +83,4 @@ app.listen(PORT, function () {
 }).on('error', err => {
   console.error(err);
 });
+
